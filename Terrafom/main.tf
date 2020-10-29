cc40e4ac7eaffb52490195b1e1b5cda921d73d54
pritamsaha72 <73169114+pritamsaha72@users.noreply.github.com>
2020-10-29 20:35:12 +1100

    Update by Pritam
    Update by Pritam

--- Terrafom/main.tf
+++ Terrafom/main.tf
@@ -62,7 +62,7 @@ resource "azurerm_virtual_machine" "main" {
     managed_disk_type = "Standard_LRS"
   }
   os_profile {
-    computer_name  = "hostname"
+    computer_name  = "TestServer"
     admin_username = "testadmin"
     admin_password = "Password1234!"
   }
